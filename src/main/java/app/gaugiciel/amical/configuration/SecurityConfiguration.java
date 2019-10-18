3f16c6966561ea1b2a9ac79383fd95284253e6fe
GregGaoter <greg.gautier@hotmail.com>
2019-10-18 09:25:30 +0200

    Ajout de visiteur_accueil.html et de visiteur_layout.html

--- src/main/java/app/gaugiciel/amical/configuration/SecurityConfiguration.java
+++ src/main/java/app/gaugiciel/amical/configuration/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package app.gaugiciel.amical.configuration;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	private DataSource dataSource;
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
 
 		// La requête de la méthode usersByUsernameQuery doit obligatoirement contenir
 		// un username (principal), un mot de passe (credentials) et un booléen (actif)
@@ -27,17 +29,43 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				"SELECT email AS principal,mot_de_passe AS credentials,actif_q FROM authentification WHERE email=?")
 				.authoritiesByUsernameQuery(
 						"SELECT authentification_email AS principal, role_role AS role FROM profil WHERE authentification_email=?")
-				.passwordEncoder(new BCryptPasswordEncoder()).rolePrefix("ROLE_");
+				.passwordEncoder(passwordEncoder()).rolePrefix("ROLE_");
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// Indique que l'authentification passe par un formulaire d'authentification
-		http.formLogin().loginPage("/login");
+	protected void configure(final HttpSecurity http) throws Exception {
+
+		http.csrf().disable();
+
+		// the order of the antMatchers() elements is significant – the more specific
+		// rules need to come first, followed by the more general ones
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMINISTRATEUR");
+		http.authorizeRequests().antMatchers("/ami/**").hasRole("AMI");
+		// http.authorizeRequests().antMatchers("/anonymous*").anonymous();
 		http.authorizeRequests().antMatchers("/").permitAll();
-		// http.authorizeRequests().antMatchers("/ami/**").hasRole("AMI");
-		// http.authorizeRequests().antMatchers("/administrateur/**").hasRole("ADMINISTRATEUR");
+		http.authorizeRequests().antMatchers("/login*").permitAll();
+		// http.authorizeRequests().anyRequest().authenticated();
+
+		http.formLogin().loginPage("/login");
+		// Specifies the URL to validate the credentials.
+		// http.formLogin().loginProcessingUrl("/perform_login");
+		// Specifies where users will go after authenticating successfully
+		// http.formLogin().defaultSuccessUrl("homepage.html", true);
+		// The URL to send users if authentication fails.
+		// http.formLogin().failureUrl("/login.html?error=true");
+		// Specifies the AuthenticationFailureHandler to use when authenticationfails.
+		// The default is redirecting to "/login?error"
+		// http.formLogin().failureHandler(authenticationFailureHandler());
+
+		// http.logout().logoutUrl("/perform_logout");
+		http.logout().deleteCookies("JSESSIONID");
+
 		http.exceptionHandling().accessDeniedPage("/403");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/app/gaugiciel/amical/controller/AccueilController.java
+++ src/main/java/app/gaugiciel/amical/controller/AccueilController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class AccueilController {
 
 	// Action par défaut appelée après le login
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return "redirect:/accueil";
+		return "redirect:/visiteur/accueil";
+	}
+
+	@RequestMapping(value = "/visiteur/accueil")
+	public String visiteurAccueil() {
+		return "visiteur_accueil";
 	}
 
 	@RequestMapping(value = "/login")
@@ -21,11 +26,6 @@ public class HomeController {
 	@RequestMapping(value = "/403")
 	public String accessDenied() {
 		return "403";
-	}
-
-	@RequestMapping(value = "/accueil")
-	public String accueil() {
-		return "accueil";
 	}
 
 }
